dc6cb9f00bda275db5af23a61749c80eb5229c61
Arman <grigoryan.arman@bk.ru>
2019-10-21 17:03:47 +0400

    change scss files names

---
+++ src/libs/doRequest.js
@@ -0,0 +1,26 @@
+import Helpers from "./helpers";
+
+const doRequest = (origin, params) => {
+  return new Promise((resolve, reject) => {
+    let query = '?';
+    const callbackName = `JQuery${Helpers.getRandomString()}`;
+
+    Object.entries(params).forEach(([key, value]) => {
+      query += `${key}=${encodeURIComponent(value)}&`;
+    });
+
+    query += `callback=${callbackName}`;
+
+    const script = document.createElement('script');
+    script.src = `${origin}${query}`;
+
+    window[callbackName] = function(res) {
+      document.body.removeChild(script);
+      delete window[callbackName];
+      resolve(res);
+    };
+    document.body.appendChild(script);
+  })
+};
+
+export default doRequest;
